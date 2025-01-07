b0b95d256bc068ad8247227877a300b3a3aa96b3
huxiaotaostasy <1979005820hux@gmail.com>
2025-01-07 20:16:29 +0800

    add nuplan dataset

--- datasets/create_dataset.py
+++ datasets/create_dataset.py
@@ -1,7 +1,7 @@
 from torch.utils.data import ConcatDataset
 
 from datasets.dataset_demo import DemoTest
-
+from datasets.dataset_nuplan import NuPlanTest
 def create_test_datasets(args, split='test'):
     data_list = args.test_data_list
     dataset_list = []
@@ -10,6 +10,11 @@ def create_test_datasets(args, split='test'):
             dataset = DemoTest(
                     args.datasets_paths['demo_root'], 
                     condition_frames=args.condition_frames)
+        elif data_name == 'nuplan':
+            dataset = NuPlanTest(
+                args.datasets_paths['nuplan_root'],
+                args.datasets_paths['nuplan_json_root'],
+                condition_frames=args.condition_frames)
         dataset_list.append(dataset)
     data_array = ConcatDataset(dataset_list)
     return data_array
